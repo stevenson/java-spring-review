0423f1a6247c9d967b4a2df0fa062bcaba2f9390
stevenson lee <stevenson.cunanan@gmail.com>
2022-05-20 17:14:32 +0800

    Update: connect postgres implementation

--- src/main/java/com/stevenson/demo/dao/PersonDataAccessService.java
+++ src/main/java/com/stevenson/demo/dao/PersonDataAccessService.java
@@ -1,12 +1,23 @@
 package com.stevenson.demo.dao;
 
 import com.stevenson.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository("postgres")
 public class PersonDataAccessService implements PersonDao{
+
+    private final JdbcTemplate jdbcTemplate;
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int create(UUID id, Person person) {
         return 0;
@@ -14,6 +25,12 @@ public class PersonDataAccessService implements PersonDao{
 
     @Override
     public List<Person> retrieveAll() {
+        final String sql = "SELECT id, name FROM person";
+        jdbcTemplate.query(sql, (resultSet, i) -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(id, name);
+        });
         return null;
     }
 
